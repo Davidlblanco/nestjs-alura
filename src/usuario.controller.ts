617e38186aa0ee9c8b2ed61b6800e347982cfe8e
David Blanco <david.lucas@maeztra.com>
2024-05-05 14:04:22 -0300

    list users

--- src/usuario.controller.ts
+++ src/usuario.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 import { UsuarioRepository } from "./usuario.repository";
 
 @Controller('/usuarios')
@@ -7,5 +7,9 @@ export class UsuarioController{
     @Post()
     async criaUsuario(@Body() dadosDoUsuario){
         return this.usuarioRepository.salvar(dadosDoUsuario)
+    }
+    @Get()
+    async listUsuarios() {
+        return this.usuarioRepository.listar()
     }
 }
